8949263cf377c67cbe4517a6c63d20cc795a382e
cudnah124 <cudnah.124@gmail.com>
2026-01-23 14:10:06 +0700

    Change preprocessing to return uint8 for memory optimization

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -3,11 +3,12 @@ import numpy as np
 from collections import deque
 
 def preprocess_frame(frame):
-    """Convert 210x160x3 RGB to 84x84x1 grayscale"""
+    """Convert 210x160x3 RGB to 84x84x1 grayscale, return uint8 to save memory"""
     gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
     resized = cv2.resize(gray, (84, 84), interpolation=cv2.INTER_AREA)
-    normalized = resized / 255.0
-    return normalized
+    # Return uint8 [0-255] instead of float32 [0-1] to save memory
+    # Normalization will be done in agent train_step on GPU
+    return resized.astype(np.uint8)
 
 class FrameStack:
     """Stack last N frames"""
@@ -27,4 +28,5 @@ class FrameStack:
         return self._get_state()
     
     def _get_state(self):
-        return np.stack(self.frames, axis=0)
+        # Stack frames as uint8, will be converted to float32 in agent
+        return np.stack(self.frames, axis=0).astype(np.uint8)
